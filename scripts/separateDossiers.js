9a408327fbb8cb1d2592ccc6f53b63dda39560f9
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-06-29 14:18:10 +0200

    add id dossier AN for future link to LFDLL

--- scripts/separateDossiers.js
+++ scripts/separateDossiers.js
@@ -1,15 +1,12 @@
 /* TODO
 --------
-- split interv's dossiers by seance
 - add metas on dossier
-- add link to LFDLL
 - add metrics by dossier
 - add metas on seance (sommaire, top orateurs, stats didascalies ..)
 - add metrics by seance ?
 - filter dossiers nearly empty/non legislative (no id_an)
 - lighten data by removing redundancy field
 */
-
 
 const dsv = require('d3-dsv');
 const fs = require('fs-extra');
@@ -19,6 +16,11 @@ const slugify = require('slugify');
 const DATA_FOLDER = path.resolve(__dirname + '/../data/');
 const OUTPUT_FOLDER = path.resolve(__dirname + '/../rififi/server/data/');
 
+const dossiersMap = dsv.tsvParse(fs.readFileSync(`${DATA_FOLDER}/dossiers_ids.tsv`, 'utf8'))
+  .filter(d => d.nb_interventions > 10)
+  .reduce((result, obj) => {
+    return Object.assign(result, { [obj.titre]: obj.id_dossier_an.replace("NULL", "") || null })
+  }, {});
 fs.ensureDir(`${OUTPUT_FOLDER}/dossiers`)
   .then(() => fs.readFile(`${DATA_FOLDER}/dossiers.json`))
   .then(dossiers => Promise.all(
@@ -26,7 +28,7 @@ fs.ensureDir(`${OUTPUT_FOLDER}/dossiers`)
       const nom = slugify(dossier.key);
       const filename = `${OUTPUT_FOLDER}/dossiers/${nom}.json`;
       console.log(filename);
-      let seances = [];
+      const seances = [];
       let curSeance = {};
       dossier.values.forEach(function(i){
         if (i.seance_id !== curSeance.id) {
@@ -40,11 +42,11 @@ fs.ensureDir(`${OUTPUT_FOLDER}/dossiers`)
         }
         curSeance.interventions.push(i);
       });
-      seances.push(curSeance)
+      seances.push(curSeance);
       let dos = {
         id: nom,
         nom: dossier.key,
-        id_an: null,
+        id_an: dossiersMap[dossier.key] || null,
         metrics: {},
         seances: seances
       };
