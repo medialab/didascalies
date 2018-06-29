f5aa1327d624b763a126e35ff5f96a973cf8dc7d
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-06-29 16:58:56 +0200

    add missing groupes for multiple unknown MPs

--- scripts/dossiers.js
+++ scripts/dossiers.js
@@ -10,6 +10,30 @@ const INPUT_INTERVENTIONS = `${DATA_FOLDER}interventions.csv`;
 const didascalies = dsv.csvParse(fs.readFileSync(INPUT_DIDASCALIES, 'utf8'));
 const interventions = dsv.csvParse(fs.readFileSync(INPUT_INTERVENTIONS, 'utf8'));
 
+const GROUPS = [
+    ['LREM', ['larem', 'lrem', 'rem'], 'en marche'],
+    ['MODEM', ['modem'], 'mouvement démocrate'],
+    ['LR', ['lr'], 'républicains'],
+    ['LFI', ['lfi', 'fi'], 'insoumise'],
+    ['UAI', ['udi', 'lc', 'uai'], 'constructifs'],
+    ['NG', ['ng'], 'nouvelle gauche'],
+    ['GDR', ['gdr'], 'gauche démocrate'],
+    ['NI', ['ni'], 'non inscrit']
+];
+
+const extractGroups = function(i) {
+  const groups = {}
+  GROUPS.forEach(x => {
+    if (i.match(new RegExp(x[2], "i")))
+      return groups[x[0]] = true;
+    x[1].forEach(y => {
+      if (i.match(new RegExp("\\b" + y + "\\b", "i")))
+        return groups[x[0]] = true;
+    });
+  });
+  return Object.keys(groups);
+};
+
 function normalizeGroupName (name) {
   if (name === 'LC') {
     return 'UAI';
@@ -41,22 +65,26 @@ const interventionsEnriched = interventions.reduce((result, line, index) => {
       )
   // filtering out technical terms
   } else if(line.parlementaire !== 'NULL' || line.nom !== 'NULL')  {
-    let groupe;
+    let groupes = [];
     if (line.fonction.trim() !== '' && line.nom === 'NULL') {
       if (line.fonction.split(',')[0].trim().match(/^présidente?$/i)) {
-        groupe = 'présidence';
+        groupes.push('présidence');
       } else {
-        groupe = 'commission';
+        groupes.push('commission');
       }
     } else if (line.parlementaire === 'NULL') {
-      groupe = 'gouvernement';
+      if (line.nom.match(/groupe/i)) {
+        groupes = extractGroups(line.nom);
+      } else {
+        groupes.push('gouvernement');
+      }
     } else {
-      groupe = normalizeGroupName(line.parlementaire_groupe_acronyme);
+      groupes.push(normalizeGroupName(line.parlementaire_groupe_acronyme));
     }
     return result.concat(
       Object.assign({}, line, {
         type: 'elocution',
-        groupes: [groupe]
+        groupes: groupes
       })
     )
   }
